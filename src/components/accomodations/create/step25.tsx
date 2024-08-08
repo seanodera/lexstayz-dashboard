029a28191181dd9be59f85579590bffd91dcb835
seanolero <seanolero@outlook.com>
2024-08-08 18:13:50 +0300

    Add price input for accommodations
    Introduced a new state for price and included it in the dispatch to setStayPartial. Added an InputNumber field for price in the form with appropriate configurations.

--- src/components/accomodations/create/step25.tsx
+++ src/components/accomodations/create/step25.tsx
@@ -11,20 +11,23 @@ export default function CreateStep25() {
     const [bedrooms, setBedrooms] = useState(0)
     const [bathrooms, setBathrooms] = useState(0)
     const [beds, setBeds] = useState(0)
-
+    const [price, setPrice] = useState(0)
     useEffect(() => {
         dispatch(setStayPartial({
             bedrooms: bedrooms,
             bathrooms: bathrooms,
             beds: beds,
+            price: price,
         }))
     }, [bathrooms, bedrooms, beds]);
 
     return <Card className={'flex justify-center'}>
         <h2>House Details</h2>
         <Form className={'w-screen text-nowrap md:max-w-sm '} layout={'vertical'}>
+            <Form.Item label={'Price'} className={'font-medium'}>
+                <InputNumber value={price} onChange={(value) => setPrice(value || 0)} min={0} step={1}/>
+            </Form.Item>
             <Form.Item label={'Bedroom'} className={'font-medium'} >
-
                 <InputNumber value={bedrooms} onChange={(value) => setBedrooms(value || 0)} min={1}/>
             </Form.Item>
             <Form.Item label={'Beds'} className={'font-medium'} >
