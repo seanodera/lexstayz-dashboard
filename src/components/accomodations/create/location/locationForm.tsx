18958ab2bceec3ba63fe296ad5f6d3681d6d5845
seanolero <seanolero@outlook.com>
2024-08-18 02:06:02 +0300

    Enable client-side rendering for location components
    Added 'use client' directive at the beginning of three files in the location creation module. These changes ensure that the components within 'locationForm.tsx', 'locationSearchBox.tsx', and 'mapWithMarker.tsx' are rendered on the client side.

--- src/components/accomodations/create/location/locationForm.tsx
+++ src/components/accomodations/create/location/locationForm.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React from "react";
 import { Field, Fieldset, Label } from "@headlessui/react";
 import { Input } from "antd";
--- src/components/accomodations/create/location/mapWithMarker.tsx
+++ src/components/accomodations/create/location/mapWithMarker.tsx
@@ -1,3 +1,4 @@
+'use client'
 import React, {useEffect, useState} from "react";
 import { MapContainer, TileLayer, Marker, useMap } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
