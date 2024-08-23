79acf40468bd745e27d621ea1f87664e1fd557cc
seanolero <seanolero@outlook.com>
2024-08-24 02:00:24 +0300

    Update cancellation handling for bookings
    Refactored code to streamline the cancellation process for bookings by separating hotel and home accommodation types. Introduced server timestamp checking and added utility methods for handling booking reversals on cancellations, ensuring accurate updates to room availability and booked dates.

--- src/slices/bookingThunks/updateBookingStatusAsync.tsx
+++ src/slices/bookingThunks/updateBookingStatusAsync.tsx
@@ -6,7 +6,8 @@ import {firestore} from "@/lib/firebase";
 import {collection, doc, getDoc} from "firebase/firestore";
 import {RootState} from "@/data/store";
 import dayjs from "dayjs";
-import {addDays} from "date-fns";
+import {addDays, isSameDay} from "date-fns";
+import {getServerTime} from "@/lib/utils";
 
 
 export const updateBookingStatusAsync = createAsyncThunk(
@@ -19,6 +20,7 @@ export const updateBookingStatusAsync = createAsyncThunk(
         const stayState = state.stay
         try {
             console.log(booking, 'At status')
+            const serverDate = await getServerTime()
             const user = getCurrentUser()
             const batch = writeBatch(firestore);
             const hostDoc = doc(firestore, 'hosts', user.uid, 'bookings', booking.id)
@@ -26,73 +28,45 @@ export const updateBookingStatusAsync = createAsyncThunk(
             const hostTransaction = doc(firestore, 'hosts', booking.hostId, 'pendingTransactions', booking.id);
             const transactionDoc = await getDoc(hostTransaction)
             const availableRef = collection(firestore, 'hosts', user.uid, 'availableTransactions')
+            const stayRef = doc(firestore, 'stays', booking.accommodationId);
             let paymentData = booking.paymentData;
             if (status === 'Rejected' || status === 'Canceled') {
                 if (booking.status === 'Pending' && booking.isConfirmed){
                     paymentData = await refundBooking(booking)
                 }
                 else if (booking.isConfirmed) {
-                    const stay = stayState.stays.find((stay) => stay.id === booking.accommodationId);
-                    console.log(stay)
-                    if (stay) {
-                        const stayDoc = doc(firestore, 'stays', booking.accommodationId);
-                        batch.update(stayDoc, {status: status});
-                        if (status === 'Rejected' || status === 'Canceled') {
-                            const newStay = {...stay};
-                            newStay.availableRooms += booking.rooms;
-                            batch.update(stayDoc, newStay);
-                        }
-
-                        if (stay.cancellation.cancellation === 'Free') {
-                            paymentData = await refundBooking(booking)
-                        } else if (stay.cancellation.cancellation === 'Other') {
-                            const cancellation = stay.cancellation
-                            let date = dayjs()
-                            console.log(cancellation)
-                            if (cancellation.preDate) {
-                                if (cancellation.timeSpace === 'Days') {
-                                    date = dayjs(booking.checkInDate).subtract(cancellation.time, 'days')
-                                } else if (cancellation.timeSpace === 'Hours') {
-                                    date = dayjs(booking.checkInDate).subtract(cancellation.time, 'hours')
-                                }
-                            } else {
-                                if (cancellation.timeSpace === 'Days') {
-                                    date = dayjs(booking.checkInDate).add(cancellation.time, 'days')
-                                } else if (cancellation.timeSpace === 'Hours') {
-                                    date = dayjs(booking.checkInDate).add(cancellation.time, 'hours')
-                                }
-                            }
-                            if (transactionDoc.exists()) {
-                                batch.delete(hostTransaction)
-                            }
-                            console.log(date)
-                            if (date.isBefore(dayjs())) {
-                                const amount = ((100 - cancellation.rate) / 100) * booking.paymentData.amount
-                                console.log(amount)
-                                paymentData = await refundBooking(booking, amount)
-                                if (transactionDoc.exists()) {
-                                    batch.set(doc(availableRef, booking.id), {
-                                        ...transactionDoc,
-                                        amount: (cancellation.rate / 100) * booking.totalPrice,
-                                        paymentData: paymentData,
-                                    })
-                                } else {
-                                    batch.set(doc(availableRef, booking.id), {
-                                        id: booking.id,
-                                        amount: (cancellation.rate / 100) * booking.totalPrice,
-                                        currency: stay.currency,
-                                        paymentData: paymentData,
-                                        date: booking.createdAt,
-                                        availableDate: date.toISOString(),
-                                    })
-                                }
-                            } else {
-                                console.log('full')
-                                paymentData = await refundBooking(booking)
-                            }
-                        }
-
+                    if (isSameDay(serverDate, booking.createdAt)) {
+                        paymentData = await refundBooking(booking)
+                    } else {
+                        paymentData = await refundBooking(booking, paymentData.amount - booking.fees * booking.paymentRate)
                     }
+
+                }
+                const staySnap = await getDoc(stayRef);
+
+                if (!staySnap.exists()) {
+                    throw new Error("Accommodation document does not exist!");
+                }
+                const stayData = staySnap.data()
+                const checkIn = new Date(booking.checkInDate);
+                const checkOut = new Date(booking.checkOutDate);
+                console.log(stayData)
+                if (stayData) {
+
+
+                    if (stayData.type === 'Hotel') {
+
+                        const {updatedRooms, fullyBookedDates} = reverseProcessHotelBooking(stayData, booking, checkIn, checkOut);
+                        console.log('Unavailable: ',updatedRooms, fullyBookedDates);
+                        batch.update(stayRef, {rooms: updatedRooms, fullyBookedDates});
+                    } else if (stayData.type === 'Home') {
+                        const unavailableDates = reverseProcessHomeBooking(stayData, checkIn, checkOut);
+                        console.log('Unavailable: ', unavailableDates)
+                        batch.update(stayRef, {bookedDates: unavailableDates});
+                    }
+
+
+
                 }
             }
 
@@ -131,3 +105,70 @@ export const updateBookingStatusAsync = createAsyncThunk(
 
     }
 );
+
+function reverseProcessHotelBooking(stayData: any, booking: any, checkIn: Date, checkOut: Date): {
+    updatedRooms: any[],
+    fullyBookedDates: string[]
+} {
+    const rooms = stayData.rooms;
+    const bookedRooms = booking.rooms;
+    const bookingIds = bookedRooms.map((room: any) => room.roomId);
+
+    let updatedRooms: any[] = [];
+    let fullyBookedDates: string[] = [...stayData.fullyBookedDates];
+
+    rooms.forEach((room: any) => {
+        if (bookingIds.includes(room.id)) {
+            const bookedRoomData = bookedRooms.find((r: any) => r.roomId === room.id);
+            let {bookedDates, fullDates} = room;
+
+            // Initialize if undefined
+            bookedDates = bookedDates ? {...bookedDates} : {};
+            fullDates = fullDates ? [...fullDates] : [];
+
+            iterateDaysBetween(checkIn, checkOut, (date) => {
+                const currentDateStr = date.toISOString().split('T')[0];
+
+                if (bookedDates[currentDateStr]) {
+                    // Decrease the booked count due to cancellation
+                    bookedDates[currentDateStr] -= bookedRoomData.numRooms;
+
+                    if (bookedDates[currentDateStr] <= 0) {
+                        delete bookedDates[currentDateStr];
+                    }
+
+                    // If room is now available on this date, remove from fullDates
+                    if (fullDates.includes(currentDateStr) && bookedDates[currentDateStr] < room.available) {
+                        fullDates = fullDates.filter((value: string) => value !== currentDateStr);
+                        fullyBookedDates = fullyBookedDates.filter((value: string) => value !== currentDateStr);
+                    }
+                }
+            });
+
+            updatedRooms.push({...room, bookedDates, fullDates});
+        } else {
+            updatedRooms.push(room);
+        }
+    });
+
+    return {updatedRooms, fullyBookedDates};
+}
+
+function reverseProcessHomeBooking(stayData: any, checkIn: Date, checkOut: Date): string[] {
+    const unavailableDates = new Set<string>(stayData.bookedDates || []);
+
+    iterateDaysBetween(checkIn, checkOut, (date) => {
+        unavailableDates.delete(date.toISOString().split('T')[0]);
+    });
+
+    return Array.from(unavailableDates);
+}
+
+function iterateDaysBetween(startDate: Date, endDate: Date, task: (date: Date) => void): void {
+    let currentDate = new Date(startDate);
+
+    while (currentDate <= endDate) {
+        task(currentDate);
+        currentDate.setDate(currentDate.getDate() + 1);
+    }
+}
